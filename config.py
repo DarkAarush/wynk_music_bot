4678b1df1578d19756d15c8aca6234e346cb59c8
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2023-03-31 08:50:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://images.hindustantimes.com/tech/rf/image_size_960x540/HT/p2/2019/11/28/Pictures/_467b713e-11d7-11ea-b3fe-5324eb805ee9.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://www.medianews4u.com/wp-content/uploads/2019/05/Tips.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://images.hindustantimes.com/tech/rf/image_size_960x540/HT/p2/2019/11/28/Pictures/_467b713e-11d7-11ea-b3fe-5324eb805ee9.jpg"
+            START_IMG_URL = "https://www.medianews4u.com/wp-content/uploads/2019/05/Tips.jpg"
